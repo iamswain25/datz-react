fb5a973c81032841b1f35e81bb8dd6cb1c433757
swain <iamswain@hotmail.com>
2020-08-31 08:47:25 +0900

    respect static media

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,5 +1,8 @@
 export const imageUrl =
   "https://raw.githubusercontent.com/iamswain25/datz-image/master/";
 export function makeUrl(url: string) {
+  if (url.indexOf("static/media") > -1) {
+    return url;
+  }
   return imageUrl + url;
 }
