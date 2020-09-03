17538bbe5f06e2756dcd283f694d63987cef5c52
swain <iamswain@hotmail.com>
2020-09-03 17:14:57 +0900

    publication item left font

--- src/components/PublicationItemLeft.tsx
+++ src/components/PublicationItemLeft.tsx
@@ -5,6 +5,7 @@ import useDesktop from "./useDesktop";
 import PublicationCloseBtn from "./PublicationCloseBtn";
 import DatzpressOrder from "./DatzpressOrder";
 import { bottomBtn37, paddingH27 } from "./styles";
+import useLang from "./useLang";
 const stickyContainer = css`
   position: sticky;
   top: 79px;
@@ -22,6 +23,7 @@ const mobileContainer = css`
 `;
 export default function PublicationItemLeft({ item }: { item: any }) {
   const isDesktop = useDesktop();
+  const [classes] = useLang("publication");
   const { title, artist, preview_quote, preview_body, order_url, id } = item;
   return (
     <div className={isDesktop ? stickyContainer : mobileContainer}>
@@ -32,95 +34,18 @@ export default function PublicationItemLeft({ item }: { item: any }) {
           display: flex;
           flex-direction: column;
           overflow: hidden;
-          padding-left: ${isDesktop ? 10 : 0}px;
-          padding-right: ${isDesktop ? 10 : 0}px;
+          padding: 0 ${isDesktop ? 10 : 0}px ${isDesktop ? 0 : 40}px;
           flex: 1;
         `}
       >
-        {item.edition && (
-          <div
-            className={css`
-              font-family: ArnoPro-Subhead;
-              font-size: 20px;
-              line-height: 1.4;
-              letter-spacing: 0.4px;
-              text-align: center;
-              color: #707070;
-              margin-top: 18px;
-            `}
-          >
-            {item.edition}
-          </div>
-        )}
+        {item.edition && <div className={classes.edition}>{item.edition}</div>}
         {item.copies_count && (
-          <div
-            className={css`
-              font-family: ArnoPro-Subhead;
-              font-size: 16px;
-              line-height: 1.38;
-              letter-spacing: 0.32px;
-              text-align: center;
-              color: #afafaf;
-              margin-top: 3px;
-            `}
-          >
-            {item.copies_count} copies
-          </div>
+          <div className={classes.copies_count}>{item.copies_count} copies</div>
         )}
-        <div
-          className={css`
-            font-family: ArnoPro-Subhead;
-            font-size: 27px;
-            line-height: 1.19;
-            letter-spacing: 0.54px;
-            text-align: center;
-            color: #4b4b4b;
-            margin-top: 28px;
-          `}
-        >
-          {title}
-        </div>
-        <div
-          className={css`
-            font-family: ArnoPro-Display;
-            font-size: 21px;
-            line-height: 1.38;
-            letter-spacing: 0.42px;
-            text-align: center;
-            color: #4b4b4b;
-            margin-top: 4px;
-          `}
-        >
-          {artist}
-        </div>
-        <div
-          className={css`
-            font-family: ArnoPro-Display;
-            font-size: 20px;
-            font-style: italic;
-            line-height: 1.35;
-            letter-spacing: 0.4px;
-            text-align: left;
-            margin-top: 29px;
-            color: #4b4b4b;
-          `}
-        >
-          {preview_quote}
-        </div>
-        <div
-          className={css`
-            font-family: ArnoPro-Display;
-            font-size: 20px;
-            line-height: 1.35;
-            letter-spacing: 0.4px;
-            text-align: left;
-            color: #4b4b4b;
-            margin-top: 30px;
-            overflow: hidden;
-          `}
-        >
-          {preview_body}
-        </div>
+        <div className={classes.title}>{title}</div>
+        <div className={classes.artist}>{artist}</div>
+        <div className={classes.quotes}>{preview_quote}</div>
+        <div className={classes.body}>{preview_body}</div>
       </div>
       <Link
         to={`/publication/${id}/readmore`}
