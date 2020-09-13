ff874f9dfb45fd7d01286340f88a201126661f8d
swain <iamswain@hotmail.com>
2020-09-13 16:30:11 +0900

    functions init

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from "firebase-functions";
+import algoliasearch from "algoliasearch";
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+const ALGOLIA_INDEX_NAME = "datzpress";
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+exports.onWrite = functions
+  .region("asia-northeast3")
+  .firestore.document("{collectionId}/{documentId}")
+  .onWrite((change, context) => {
+    const { documentId, collectionId } = context.params;
+    switch (collectionId) {
+      case "publication":
+      case "artist":
+      case "exhibition":
+      case "event":
+        break;
+      default:
+        return null;
+    }
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    if (change.after.exists) {
+      const data = change.after.data();
+      if (!data) return new Error("no data");
+      // Add an 'objectID' field which Algolia requires
+      data.objectID = documentId;
+      return index.saveObject(data);
+    } else {
+      return index.deleteObject(documentId);
+    }
+  });
