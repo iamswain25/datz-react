910906c7afdd95ee4cdc1705a0249b40bfc2d24e
swain <iamswain@hotmail.com>
2020-09-20 00:24:05 +0900

    firestore: contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,6 +5,11 @@ import AboutHeader from "../components/AboutHeader";
 import { marginH55, flexcolumncenter, marginH27 } from "../components/styles";
 import BtnTop from "../components/BtnTop";
 import Divider from "../components/Divider";
+import { Grid } from "@material-ui/core";
+import useItems from "../utils/useItems";
+import useLang from "../components/useLang";
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
+import { firestore } from "../config/firebase";
 
 const aStyle = css`
   text-decoration: underline;
@@ -12,45 +17,52 @@ const aStyle = css`
   line-height: 1.39;
   font-size: 18px;
 `;
-const collectionList = [
-  ["Stanford Libraries", "/"],
-  ["Amon Carter Museum ", "/"],
-  ["ICP Library ", "/"],
-  ["Special Collections, John M. Flaxman Library (SAIC)  ", "/"],
-  ["SFAI Library   ", "/"],
-  ["New York Public Library", "/"],
-  ["Duke University Libraries", "/"],
-];
-function mapLinks(top: any, i: number) {
-  if (typeof top === "string") {
-    return <div key={i}>{top}</div>;
-  }
-  if (top instanceof Array) {
-    const [label, link] = top;
-    return (
-      <a href={link} key={i} className={aStyle}>
-        {label}
-      </a>
-    );
-  } else {
-    return <br key={i} />;
-  }
-}
+const h1Style = css`
+  font-family: BauerGroteskOTW03;
+  font-size: 17px;
+  line-height: 1.24;
+  padding-bottom: 6px;
+  margin-bottom: 12px;
+  border-bottom: 1px solid #fff;
+  width: 100%;
+`;
+const mapLinks = (en: boolean = false) => (stock: any, i: number) => {
+  const { title, list } = stock;
+  return (
+    <div key={i}>
+      <span>{title}</span>
+      {list?.map((l: any, i2: number) => (
+        <a
+          href={"http://" + l.url}
+          key={i2}
+          className={aStyle}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {en ? l.title_en : l.title_ko}
+        </a>
+      ))}
+      <br />
+    </div>
+  );
+};
 export default function Contact() {
   const isDesktop = useDesktop(true);
-  const h1Style = React.useMemo(
-    () => css`
-      font-family: BauerGroteskOTW03;
-      font-size: 17px;
-      line-height: 1.24;
-      padding-bottom: 6px;
-      margin-bottom: 12px;
-      margin-top: ${isDesktop ? 0 : 74}px;
-      border-bottom: 1px solid #fff;
-      width: 100%;
-    `,
-    [isDesktop]
+  const [data] = useCollectionDataOnce(
+    firestore.collection("contact").orderBy("order", "asc")
   );
+  const items = useItems(data);
+  const [, en] = useLang();
+  const list = React.useMemo(() => {
+    if (!items) return;
+    const enquiry = items.find((i) => i.type === "enquiry");
+    const phone = items.find((i) => i.type === "phone");
+    const email = items.find((i) => i.type === "email");
+    const catalog = items.find((i) => i.type === "catalog");
+    const stocklist = items.filter((i) => i.type === "stocklist");
+    const collections = items.filter((i) => i.type === "collections");
+    return { stocklist, enquiry, collections, phone, email, catalog };
+  }, [items]);
   return (
     <>
       <div
@@ -59,36 +71,29 @@ export default function Contact() {
           font-family: BauerGroteskOTW03-Regular;
           text-align: center;
           color: #ffffff;
-          height: ${isDesktop ? "100vh" : "auto"};
         `}
       >
         <AboutHeader sticky />
         <div
           className={css`
-            display: ${isDesktop ? "grid" : "flex"};
+            display: flex;
             flex-direction: column;
-            grid-template-columns: repeat(2, 1fr);
-            grid-template-rows: repeat(7, 1fr);
-            column-gap: 62px;
-            height: ${isDesktop ? "calc(100vh - 79px - 37px)" : "auto"};
             min-height: 600px;
             border-bottom: 1px solid #fff;
             padding-bottom: 20px;
             ${isDesktop ? marginH55 : marginH27}
           `}
         >
-          <div
+          <section
             className={css`
               ${flexcolumncenter}
-              grid-column: 1/3;
-              grid-row: 1;
               align-items: stretch;
+              margin: 32px 0;
             `}
           >
             <h1
               className={css`
                 font-family: BauerGroteskOTW03;
-                margin-top: 32px;
                 font-size: 23px;
                 line-height: 1.17;
               `}
@@ -102,182 +107,103 @@ export default function Contact() {
               `}
             />
             <a
-              href="mailto:datzpress@datzpress.com"
+              href={"mailto:" + list?.enquiry?.title}
               target="_blank"
               rel="noopener noreferrer"
               className={css`
                 font-size: 16px;
                 line-height: 1.19;
+                margin-top: 12px;
               `}
             >
-              datzpress@datzpress.com
+              {list?.enquiry?.title}
             </a>
-          </div>
-          <div
+          </section>
+          <section
             className={css`
-              grid-column: 1;
-              grid-row: 2/7;
+              margin: 32px 0;
             `}
           >
-            <h1 className={h1Style}>STOCKLIST</h1>
-            <div
-              className={css`
-                grid-template-columns: repeat(2, 1fr);
-                display: grid;
-                font-size: 18px;
-                line-height: 1.39;
-              `}
-            >
-              <div>
-                {[
-                  "Seoul, Korea",
-                  ["MMCA Art Zone", "/"],
-                  ["Your-mind", "/"],
-                  ["The Reference", "/"],
-                  ["Photobooks Gorae", "/"],
-                  null,
-                  "New York, USA",
-                  ["Printed Matter Inc.", "/"],
-                  ["Clic Gallery", "/"],
-                  null,
-                  "New Mexico, USA",
-                  ["Photo-eye Bookstore", "/"],
-                ].map(mapLinks)}
-              </div>
-              <div>
-                {[
-                  "San Francisco, USA",
-                  ["Smith Anderson Gallery", "/"],
-                  ["Moe’s Books", "/"],
-                  null,
-                  "Hawaii, USA, USA",
-                  ["Honolulu Museum of Art", "/"],
-                  ["Tree House", "/"],
-                  ["Minny Lee Fine Art. LLC", "/"],
-                  null,
-                  "Massachusetts, USA",
-                  ["Concord Art Center", "/"],
-                  null,
-                  "Taipei City, Taiwan",
-                  ["Artland Bookstore", "/"],
-                ].map(mapLinks)}
-              </div>
-            </div>
-          </div>
-          <div
-            className={css`
-              grid-column: 1;
-              grid-row: 7;
-              display: flex;
-              flex-direction: column;
-              justify-content: flex-end;
-            `}
-          >
-            <h1 className={h1Style}>CATALOG</h1>
-            <a href="/publication" className={aStyle}>
-              Publication 2019
-            </a>
-          </div>
-          <div
-            className={css`
-              grid-column: 2;
-              grid-row: 2/5;
-            `}
-          >
-            <h1 className={h1Style}>COLLECTIONS</h1>
-            {collectionList.map(mapLinks)}
-          </div>
-          <div
-            className={css`
-              grid-column: 2;
-              grid-row: 5/8;
-              display: flex;
-              flex-direction: column;
-              justify-content: flex-end;
-            `}
-          >
-            <h1 className={h1Style}>CONTACT</h1>
-            <div
-              className={css`
-                line-height: 1.39;
-                font-size: 18px;
-                white-space: break-spaces;
-              `}
-            >
-              <div>Phone</div>
-              <div>
-                <a href="tel:+82 2 447 2581">+82 2 447 2581</a>
-              </div>
-              <div>-</div>
-              <div>Email</div>
-              <div>
-                <span
+            <Grid container spacing={isDesktop ? 4 : 0}>
+              <Grid item xs={12} sm={6}>
+                <h1 className={h1Style}>STOCKLIST</h1>
+                <div
                   className={css`
-                    margin-right: 16px;
+                    font-size: 18px;
+                    line-height: 1.39;
                   `}
                 >
-                  Datz Press
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="datzpress@datzpress.com"
-                >
-                  datzpress@datzpress.com
-                </a>
-              </div>
-              <div>
-                <span
+                  {list?.stocklist?.map(mapLinks(en))}
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <div>
+                  <h1 className={h1Style}>COLLECTIONS</h1>
+                  {list?.collections?.map(mapLinks(en))}
+                </div>
+                <div
                   className={css`
-                    margin-right: 16px;
+                    margin: 32px 0;
                   `}
                 >
-                  Datz Books
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="books@datzpress.com"
-                >
-                  books@datzpress.com
-                </a>
-              </div>
-              <div>
-                <span
+                  <h1 className={h1Style}>CONTACT</h1>
+                  <div
+                    className={css`
+                      line-height: 1.39;
+                      font-size: 18px;
+                      white-space: break-spaces;
+                    `}
+                  >
+                    <div>{list?.phone?.title}</div>
+                    <div>
+                      <a href={"tel:" + list?.phone?.text}>
+                        {list?.phone?.text}
+                      </a>
+                    </div>
+                    <div>-</div>
+                    <div>{list?.email?.title}</div>
+                    {list?.email?.list.map(
+                      ({ name, email }: any, key: number) => (
+                        <div key={key}>
+                          <span
+                            className={css`
+                              margin-right: 16px;
+                            `}
+                          >
+                            {name}
+                          </span>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={"mailto:" + email}
+                          >
+                            {email}
+                          </a>
+                        </div>
+                      )
+                    )}
+                  </div>
+                </div>
+                <div
                   className={css`
-                    margin-right: 16px;
+                    margin: 32px 0;
                   `}
                 >
-                  D’Ark Room
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="darkroom@datzpress.com"
-                >
-                  darkroom@datzpress.com
-                </a>
-              </div>
-              <div>
-                <span
-                  className={css`
-                    margin-right: 16px;
-                  `}
-                >
-                  DMA
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="darkroom@datzpress.com"
-                >
-                  museum@datzpress.com
-                </a>
-              </div>
-            </div>
-          </div>
+                  <h1 className={h1Style}>CATALOG</h1>
+                  <a
+                    href={list?.catalog?.url}
+                    className={aStyle}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {list?.catalog?.title}
+                  </a>
+                </div>
+              </Grid>
+            </Grid>
+          </section>
         </div>
-        {!isDesktop && <BtnTop color="white" />}
+        <BtnTop color="white" full />
       </div>
     </>
   );
