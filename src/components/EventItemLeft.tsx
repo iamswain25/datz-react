c33b5aae90aaff2e3f724d45b4c640ee15adfd1e
swain <iamswain@hotmail.com>
2020-10-15 17:06:52 +0900

    ie11: fix sticky left

--- src/components/EventItemLeft.tsx
+++ src/components/EventItemLeft.tsx
@@ -3,9 +3,10 @@ import { css } from "emotion";
 import useDesktop from "./useDesktop";
 import EventCoverWidget from "./EventCoverWidget";
 import { flexcolumn } from "./styles";
+import { Sticky } from "react-sticky";
 const stickyContainer = css`
   position: fixed;
-position: sticky;
+  position: sticky;
   top: 79px;
   width: calc(50% - 20px);
   height: calc(100vh - 79px);
@@ -23,8 +24,15 @@ const mobileContainer = css`
 export default function EventItemLeft({ images }: { images: string[] }) {
   const isDesktop = useDesktop();
   return (
-    <div className={isDesktop ? stickyContainer : mobileContainer}>
-      <EventCoverWidget images={images} type="event" />
-    </div>
+    <Sticky>
+      {(props) => (
+        <div
+          className={isDesktop ? stickyContainer : mobileContainer}
+          style={props.style}
+        >
+          <EventCoverWidget images={images} type="event" />
+        </div>
+      )}
+    </Sticky>
   );
 }
--- src/components/PublicationItemLeft.tsx
+++ src/components/PublicationItemLeft.tsx
@@ -6,8 +6,8 @@ import PublicationCloseBtn from "./PublicationCloseBtn";
 import DatzpressOrder from "./DatzpressOrder";
 import { bottomBtn37, paddingH27 } from "./styles";
 import useLang from "./useLang";
+import { Sticky } from "react-sticky";
 const stickyContainer = css`
-  position: fixed;
   position: sticky;
   top: 79px;
   width: 350px;
@@ -27,37 +27,46 @@ export default function PublicationItemLeft({ item }: { item: any }) {
   const [classes, en] = useLang("publication");
   const { title, artist, preview_quote, preview_body, order_url, id } = item;
   return (
-    <div className={isDesktop ? stickyContainer : mobileContainer}>
-      <PublicationCloseBtn />
-      <DatzpressOrder order={order_url} />
-      <div
-        className={css`
-          display: flex;
-          flex-direction: column;
-          overflow: hidden;
-          padding: 0 ${isDesktop ? 10 : 0}px ${isDesktop ? 0 : 40}px;
-          flex: 1;
-          color: ${!en ? "#5d5d5d" : "#4b4b4b"};
-        `}
-      >
-        {item.edition && <div className={classes.edition}>{item.edition}</div>}
-        {item.copies_count && (
-          <div className={classes.copies_count}>{item.copies_count}</div>
-        )}
-        <div className={classes.title}>{title}</div>
-        <div className={classes.artist}>{artist}</div>
-        <div className={classes.quotes}>{preview_quote}</div>
-        <div className={classes.body}>{preview_body}</div>
-      </div>
-      <Link
-        to={`/publication/${id}/readmore`}
-        className={css`
-          border-top: solid 1px #707070;
-          ${bottomBtn37}
-        `}
-      >
-        read more {">"}
-      </Link>
-    </div>
+    <Sticky>
+      {(props) => (
+        <div
+          className={isDesktop ? stickyContainer : mobileContainer}
+          style={props.style}
+        >
+          <PublicationCloseBtn />
+          <DatzpressOrder order={order_url} />
+          <div
+            className={css`
+              display: flex;
+              flex-direction: column;
+              overflow: hidden;
+              padding: 0 ${isDesktop ? 10 : 0}px ${isDesktop ? 0 : 40}px;
+              flex: 1;
+              color: ${!en ? "#5d5d5d" : "#4b4b4b"};
+            `}
+          >
+            {item.edition && (
+              <div className={classes.edition}>{item.edition}</div>
+            )}
+            {item.copies_count && (
+              <div className={classes.copies_count}>{item.copies_count}</div>
+            )}
+            <div className={classes.title}>{title}</div>
+            <div className={classes.artist}>{artist}</div>
+            <div className={classes.quotes}>{preview_quote}</div>
+            <div className={classes.body}>{preview_body}</div>
+          </div>
+          <Link
+            to={`/publication/${id}/readmore`}
+            className={css`
+              border-top: solid 1px #707070;
+              ${bottomBtn37}
+            `}
+          >
+            read more {">"}
+          </Link>
+        </div>
+      )}
+    </Sticky>
   );
 }
