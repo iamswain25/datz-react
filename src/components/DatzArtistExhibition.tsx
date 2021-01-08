1309e34708f4fd5364a92f23c2a871cf415f8fef
swain <iamswain@hotmail.com>
2021-01-09 01:05:55 +0900

    추가 로고

--- src/components/DatzArtistExhibition.tsx
+++ src/components/DatzArtistExhibition.tsx
@@ -7,6 +7,8 @@ import DarkroomLong from "../assets/svg/DarkroomLong";
 import DatzMuseum from "../assets/svg/DatzMuseum";
 import DmaKo from "../assets/svg/DmaKo";
 import Dfrontspace from "../assets/svg/Dfrontspace";
+import Instagram from "../assets/svg/Instagram";
+import Fb from "../assets/svg/Fb";
 const dividerV = (className = "") => (
   <div
     className={css`
@@ -29,10 +31,8 @@ function logo(key: number) {
             display: flex;
             align-items: center;
             height: 71px;
-            margin-left: 37px;
-            margin-right: 37px;
-            padding-left: 30px;
-            padding-right: 30px;
+            margin: 0 37px;
+            padding: 0 30px;
             justify-content: center;
             overflow: hidden;
           `}
@@ -60,10 +60,8 @@ function logo(key: number) {
             display: flex;
             align-items: center;
             height: 71px;
-            margin-left: 37px;
-            margin-right: 37px;
-            padding-left: 30px;
-            padding-right: 30px;
+            margin: 0 37px;
+            padding: 0 30px;
             justify-content: center;
             overflow: hidden;
           `}
@@ -79,6 +77,121 @@ function logo(key: number) {
               height: 33px;
             `}
           />
+        </div>
+      );
+    default:
+      return <DatzSvgs color="white" />;
+  }
+}
+function links(key: number) {
+  switch (key) {
+    case 0:
+      return null;
+    case 1:
+      return (
+        <div
+          className={css`
+            display: flex;
+            align-items: center;
+            font-size: 16px;
+            margin: 27px 0 0;
+            padding: 0 30px;
+            justify-content: center;
+            overflow: hidden;
+            pointer-events: auto;
+          `}
+        >
+          <a
+            href="https://www.facebook.com/datzmuseum"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Fb
+              color="white"
+              className={css`
+                width: 24px;
+                height: 24px;
+                margin-right: 23px;
+              `}
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/datzmuseum"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Instagram
+              color="white"
+              className={css`
+                width: 24px;
+                height: 24px;
+                margin-right: 34px;
+              `}
+            />
+          </a>
+
+          <a
+            href="https://www.datzmuseum.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css`
+              text-decoration: underline;
+            `}
+          >
+            Visit DMA {">"}
+          </a>
+        </div>
+      );
+    case 2:
+      return (
+        <div
+          className={css`
+            display: flex;
+            align-items: center;
+            font-size: 16px;
+            margin: 27px 0 0;
+            padding: 0 30px;
+            justify-content: center;
+            overflow: hidden;
+            pointer-events: auto;
+          `}
+        >
+          <a
+            href="https://www.instagram.com/datzmuseum"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css``}
+          >
+            <Instagram
+              color="white"
+              className={css`
+                width: 24px;
+                height: 24px;
+                margin-right: 34px;
+              `}
+            />
+          </a>
+          <a
+            href="/exhibitions/darkroom"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css`
+              text-decoration: underline;
+              margin-right: 25px;
+            `}
+          >
+            Past Exhibition {">"}
+          </a>
+          <a
+            href="/events/all"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css`
+              text-decoration: underline;
+            `}
+          >
+            Past Event {">"}
+          </a>
         </div>
       );
     default:
@@ -160,6 +273,7 @@ export default function DatzArtistExhibition({ item }: { item: any }) {
             {text2}
           </div>
         </div>
+        {links(item.key)}
       </div>
     </div>
   );
