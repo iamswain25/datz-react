5cedc9f2793838722ee824c9384d5ff5eeed3e21
Swain Hwang <iamswain@hotmail.com>
2021-01-16 14:38:15 +0900

    ie: top notice bar

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -48,14 +48,16 @@ export default function Notice() {
         align-items: center;
         color: #383838;
         font-size: 16px;
+        position: relative;
       `}
     >
       <div
         className={css`
           position: absolute;
           left: 50%;
+          top: 50%;
           text-align: center;
-          transform: translateX(-50%);
+          transform: translate(-50%, -50%);
           overflow: hidden;
         `}
       >
