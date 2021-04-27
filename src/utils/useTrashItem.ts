157f62032d6d9cb1a497a22630dad09b337e5340
swain <iamswain@hotmail.com>
2021-04-27 20:25:37 +0900

    last update on remove

--- src/utils/useTrashItem.ts
+++ src/utils/useTrashItem.ts
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { Param } from "../@type/admin";
 import { firestore } from "../config/firebase";
 import { useAdminItem } from "../store/useGlobalState";
+import lastAdminWrite from "./lastAdminWrite";
 async function trash(item: any) {
   const { id, ...rest } = item;
   return firestore.collection("trash").doc(id).set(rest);
@@ -17,15 +18,17 @@ export default function useTrashItem(item: any) {
     case "publication_category":
       return function trash_remove() {
         if (!window.confirm("삭제하겠습니까?")) return;
-        trash(item);
-        remove(type, item.id);
+        Promise.all([trash(item), remove(type, item.id), lastAdminWrite()]);
         setItem(undefined);
       };
     default:
       return function trash_remove() {
         if (!window.confirm("삭제하겠습니까?")) return;
-        trash(item);
-        remove(collection, item.id);
+        Promise.all([
+          trash(item),
+          remove(collection, item.id),
+          lastAdminWrite(),
+        ]);
         setItem(undefined);
       };
   }
