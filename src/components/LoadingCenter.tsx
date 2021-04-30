726abb0e0b8070ab731552c9eb6ce70c478bdee0
swain <iamswain@hotmail.com>
2021-04-30 23:10:14 +0900

    업로드 중에 freeze 시키기

--- src/components/LoadingCenter.tsx
+++ src/components/LoadingCenter.tsx
@@ -4,16 +4,28 @@ import React from "react";
 
 export default function LoadingCenter() {
   return (
-    <div
+    <aside
       className={css`
         position: fixed;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        color: #fff;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #000;
+        opacity: 0.3;
       `}
     >
-      <CircularProgress color="inherit" />
-    </div>
+      <div
+        className={css`
+          position: fixed;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          color: #fff;
+        `}
+      >
+        <CircularProgress color="inherit" />
+      </div>
+    </aside>
   );
 }
