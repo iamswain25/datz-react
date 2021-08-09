50752e81ca2a15b6588b3dd675f9005385960d62
swain <>
2021-08-09 22:15:22 +0900

    code-splitting

--- src/config/Routes.tsx
+++ src/config/Routes.tsx
@@ -1,114 +1,109 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   Switch,
   Route,
   BrowserRouter as Router,
   Redirect,
 } from "react-router-dom";
-import Home from "../pages/Home";
-import Signin from "../pages/Signin";
-import Publication from "../pages/Publication";
-import PublicationItem from "../pages/PublicationItem";
-import Event from "../pages/Event";
-import EventItem from "../pages/EventItem";
-import PublicationReadmore from "../pages/PublicationReadmore";
-import PrivateRoute from "../components/PrivateRoute";
-import ArtistPage from "../pages/ArtistPage";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import AboutDatzpress from "../pages/AboutDatzpress";
-import AboutDatzmuseum from "../pages/AboutDatzmuseum";
-import AboutDarkroom from "../pages/AboutDarkroom";
-import Events from "../pages/Events";
-import Exhibition from "../pages/Exhibition";
-import Exhibitions from "../pages/Exhibitions";
-import ExhibitionItem from "../pages/ExhibitionItem";
-import ExhibitionReadmore from "../pages/ExhibitionReadmore";
-import ArtistProject from "../pages/ArtistProject";
-import Support from "../pages/Support";
-import News from "../pages/News";
-import NewsItem from "../pages/NewsItem";
-import FullImageGallery from "../pages/FullImageGallery";
 import Layout from "../components/Layout";
-import Search from "../pages/Search";
-import RoutesAdmin from "./RoutesAdmin";
+import LoadingCenter from "../components/LoadingCenter";
+import Home from "../pages/Home";
+const Signin = React.lazy(() => import("../pages/Signin"));
+const Publication = React.lazy(() => import("../pages/Publication"));
+const PublicationItem = React.lazy(() => import("../pages/PublicationItem"));
+const Event = React.lazy(() => import("../pages/Event"));
+const EventItem = React.lazy(() => import("../pages/EventItem"));
+const PublicationReadmore = React.lazy(
+  () => import("../pages/PublicationReadmore")
+);
+const PrivateRoute = React.lazy(() => import("../components/PrivateRoute"));
+const ArtistPage = React.lazy(() => import("../pages/ArtistPage"));
+const About = React.lazy(() => import("../pages/About"));
+const Contact = React.lazy(() => import("../pages/Contact"));
+const AboutDatzpress = React.lazy(() => import("../pages/AboutDatzpress"));
+const AboutDatzmuseum = React.lazy(() => import("../pages/AboutDatzmuseum"));
+const AboutDarkroom = React.lazy(() => import("../pages/AboutDarkroom"));
+const Events = React.lazy(() => import("../pages/Events"));
+const Exhibition = React.lazy(() => import("../pages/Exhibition"));
+const Exhibitions = React.lazy(() => import("../pages/Exhibitions"));
+const ExhibitionItem = React.lazy(() => import("../pages/ExhibitionItem"));
+const ExhibitionReadmore = React.lazy(
+  () => import("../pages/ExhibitionReadmore")
+);
+const ArtistProject = React.lazy(() => import("../pages/ArtistProject"));
+const Support = React.lazy(() => import("../pages/Support"));
+const News = React.lazy(() => import("../pages/News"));
+const NewsItem = React.lazy(() => import("../pages/NewsItem"));
+const FullImageGallery = React.lazy(() => import("../pages/FullImageGallery"));
+const Search = React.lazy(() => import("../pages/Search"));
+const RoutesAdmin = React.lazy(() => import("./RoutesAdmin"));
+
 export default function Routes() {
   return (
     <Router>
       <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/publication" component={Publication} />
-          <Route exact path="/exhibition" component={Exhibition} />
-          <Route exact path="/publication/:id" component={PublicationItem} />
-          <Route exact path="/:type/:id/images" component={FullImageGallery} />
+        <Suspense fallback={<LoadingCenter />}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/publication" component={Publication} />
+            <Route exact path="/exhibition" component={Exhibition} />
+            <Route exact path="/publication/:id" component={PublicationItem} />
+            <Route
+              exact
+              path="/:type/:id/images"
+              component={FullImageGallery}
+            />
 
-          <Route exact path="/event" component={Event} />
-          <Route exact path="/events">
-            <Redirect to="/events/all" />
-          </Route>
-          <Route exact path="/Exhibitions">
-            <Redirect to="/exhibitions/all" />
-          </Route>
+            <Route exact path="/event" component={Event} />
+            <Route exact path="/events">
+              <Redirect to="/events/all" />
+            </Route>
+            <Route exact path="/Exhibitions">
+              <Redirect to="/exhibitions/all" />
+            </Route>
 
-          <Route exact path="/event/:id" component={EventItem} />
-          {/* <PrivateRoute exact path="/admin/event/:id" component={EventItem} /> */}
-          {/* <PrivateRoute
-            exact
-            path="/admin/publication/:id/readmore"
-            component={PublicationReadmore}
-          /> */}
-          {/* <PrivateRoute
-            exact
-            path="/admin/exhibition/:id/readmore"
-            component={ExhibitionReadmore}
-          /> */}
-          {/* <PrivateRoute exact path="/admin/news/:id" component={NewsItem} /> */}
-          <Route exact path="/exhibition/:id" component={ExhibitionItem} />
-          <Route
-            exact
-            path="/publication/:id/readmore"
-            component={PublicationReadmore}
-          />
-          <Route
-            exact
-            path="/exhibition/:id/readmore"
-            component={ExhibitionReadmore}
-          />
-          <Route exact path="/artist/:id" component={ArtistPage} />
-          <Route exact path="/artist-project" component={ArtistProject} />
-          {/* <PrivateRoute
-            exact
-            path="/admin/artist-project"
-            component={ArtistProject}
-          /> */}
-          <Route exact path="/login" component={Signin} />
-          <Route exact path="/events/:filter" component={Events} />
-          <Route exact path="/exhibitions/:filter" component={Exhibitions} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/about/datzpress" component={AboutDatzpress} />
-          <Route exact path="/about/darkroom" component={AboutDarkroom} />
-          <Route exact path="/about/datzmuseum" component={AboutDatzmuseum} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/search/:filter/:query" component={Search} />
-          <Route exact path="/search/:filter" component={Search} />
-          <Route exact path="/search">
-            <Redirect to="/search/all/" />
-          </Route>
-          <Route exact path="/support" component={Support} />
-          <Route exact path="/newslist">
-            <Redirect to="/newslist/all" />
-          </Route>
-          <Route exact path="/news">
-            <Redirect to="/newslist/all" />
-          </Route>
-          <Route exact path="/newslist/:filter" component={News} />
-          <Route exact path="/news/:id" component={NewsItem} />
-          <PrivateRoute path="/admin" component={RoutesAdmin} />
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
+            <Route exact path="/event/:id" component={EventItem} />
+            <Route exact path="/exhibition/:id" component={ExhibitionItem} />
+            <Route
+              exact
+              path="/publication/:id/readmore"
+              component={PublicationReadmore}
+            />
+            <Route
+              exact
+              path="/exhibition/:id/readmore"
+              component={ExhibitionReadmore}
+            />
+            <Route exact path="/artist/:id" component={ArtistPage} />
+            <Route exact path="/artist-project" component={ArtistProject} />
+            <Route exact path="/login" component={Signin} />
+            <Route exact path="/events/:filter" component={Events} />
+            <Route exact path="/exhibitions/:filter" component={Exhibitions} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/about/datzpress" component={AboutDatzpress} />
+            <Route exact path="/about/darkroom" component={AboutDarkroom} />
+            <Route exact path="/about/datzmuseum" component={AboutDatzmuseum} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/search/:filter/:query" component={Search} />
+            <Route exact path="/search/:filter" component={Search} />
+            <Route exact path="/search">
+              <Redirect to="/search/all/" />
+            </Route>
+            <Route exact path="/support" component={Support} />
+            <Route exact path="/newslist">
+              <Redirect to="/newslist/all" />
+            </Route>
+            <Route exact path="/news">
+              <Redirect to="/newslist/all" />
+            </Route>
+            <Route exact path="/newslist/:filter" component={News} />
+            <Route exact path="/news/:id" component={NewsItem} />
+            <PrivateRoute path="/admin" component={RoutesAdmin} />
+            <Route path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </Router>
   );
