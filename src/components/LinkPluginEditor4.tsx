33655d5c85fb33c9d52040bddd34f5d7f3aeca78
swain <iamswain@hotmail.com>
2022-10-24 03:07:42 +0900

    fix default anchor

--- src/components/LinkPluginEditor4.tsx
+++ src/components/LinkPluginEditor4.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import createInlineToolbarPlugin from "@draft-js-plugins/inline-toolbar";
-import createLinkPlugin from "@draft-js-plugins/anchor";
+import createLinkPlugin, { defaultTheme } from "@draft-js-plugins/anchor";
 import PluginEditor, {
   createEditorStateWithText,
 } from "@draft-js-plugins/editor";
@@ -9,40 +9,47 @@ import { useDebouncedCallback } from "use-debounce";
 import "@draft-js-plugins/inline-toolbar/lib/plugin.css";
 import "@draft-js-plugins/anchor/lib/plugin.css";
 import { css } from "emotion";
-import { decorators } from "../utils/decorator";
-
 const incomingConvert = (value: any) => {
+  // console.log("incomingConvert", value);
   if (typeof value === "object") {
-    return EditorState.createWithContent(convertFromRaw(value), decorators);
+    return EditorState.createWithContent(convertFromRaw(value));
   } else {
     return createEditorStateWithText(value || "");
   }
 };
-
 export default function LinkPluginEditor4({
   value,
   onChange,
   visible,
   keyup,
 }: any) {
+  const editorRef = React.useRef<PluginEditor>(null);
   const { InlineToolbar, plugins, linkPlugin } = React.useMemo(() => {
     const inlineToolbarPlugin = createInlineToolbarPlugin();
     const { InlineToolbar } = inlineToolbarPlugin;
     const linkPlugin = createLinkPlugin({
       placeholder: "https://",
       linkTarget: "_blank",
+      theme: {
+        ...defaultTheme,
+        link: css`
+          text-decoration: underline;
+        `,
+      },
     });
     const plugins = [inlineToolbarPlugin, linkPlugin];
     return { InlineToolbar, plugins, linkPlugin };
   }, []);
-  const [state, setState] = React.useState<EditorState>(incomingConvert(value));
-  const editor = React.useRef<PluginEditor>(null);
+  const [state, setState] = React.useState<EditorState>(() =>
+    incomingConvert(value)
+  );
+
   React.useEffect(() => {
     if (
       JSON.stringify(value) !==
       JSON.stringify(convertToRaw(state.getCurrentContent()))
     ) {
-      // console.log("CHANGE", JSON.stringify(value));
+      console.log("useEffect");
       setState(incomingConvert(value));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,19 +58,17 @@ export default function LinkPluginEditor4({
     const json = JSON.stringify(convertToRaw(state.getCurrentContent()));
     // console.log(json);
     if (JSON.stringify(value) !== json) {
-      // console.log("SAVE", json);
+      console.log("saveCb");
       onChange(state.getCurrentContent());
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, 500);
-  React.useEffect(() => saveCb(), [state, saveCb]);
-
-  const focus = (): void => editor.current?.focus();
   React.useEffect(() => {
-    if (visible) {
-      focus();
+    if (!state.getDecorator()) {
+      editorRef.current?.componentDidMount();
     }
-  }, [visible]);
+    saveCb();
+  }, [state, saveCb]);
   return (
     <div
       className={css`
@@ -72,13 +77,12 @@ export default function LinkPluginEditor4({
         display: ${visible ? "block" : "none"};
         color: #707070;
       `}
-      onClick={focus}
     >
       <PluginEditor
+        ref={editorRef}
         editorState={state}
         onChange={setState}
         plugins={plugins}
-        ref={editor}
         stripPastedStyles={true}
         keyBindingFn={keyup}
       />
